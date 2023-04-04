3cfd59ea1464faf606b666de7e4fdcdee84806a2
Julien <julien.abid@etu.unistra.fr>
2023-04-04 23:51:17 +0200

    Ajout commentaire index.js

--- projet/src/index.js
+++ projet/src/index.js
@@ -1,6 +1,14 @@
+// Description: This file is the entry point of the game
+// It creates the game and the main scene
+// It also imports all the necessary resources
 import Phaser from 'phaser';
 import MainScene from './scenes/mainScene';
 
+// Creating the game
+// The game will be 960x720 pixels
+// The game will use the WebGL renderer
+// The game will use the Arcade physics engine
+// The game will use the MainScene scene
 const config = {
     type: Phaser.AUTO,
     width: window.innerWidth,
