cf1250534d25a05dcb14325a1f5c582120c0056d
Pett Vincent <vincent.pett@etu.unistra.fr>
2023-04-05 16:18:56 +0200

    Ajout des pnj dans la scène 3

--- projet/src/scenes/thirdGameScene.js
+++ projet/src/scenes/thirdGameScene.js
@@ -1,6 +1,9 @@
 import Phaser from 'phaser';
 import bc from '../assets/img/bg_3.png';
 import Player from '../model/player';
+import PNJ from '../model/pnj';
+import pnjImg from '../assets/img/pnj.png';
+
 
 
 export default class ThirdGameScene extends Phaser.Scene {
@@ -12,8 +15,8 @@ export default class ThirdGameScene extends Phaser.Scene {
     preload() {
         this.load.image('bgscene', bc);
         Player.loadAssets(this);
-
-
+        this.load.image('pnj', pnjImg);
+        PNJ.loadAssets(this);
     }
 
     create() {
@@ -23,6 +26,13 @@ export default class ThirdGameScene extends Phaser.Scene {
     this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bgscene');
     this.player = new Player(this, 100, 450);
 
+    this.pnj6 = new PNJ(this,640,450,'pnj',"Me defierez-vous à une partie de pétanque !?");
+    this.pnj6 = new PNJ(this,130,250,'pnj',"J'ai entendu dire que vous avez décidé de consacrer votre vie à la spiritualité. Seriez vous intéressé pas le fait de devenir moine ?");
+    this.pnj6 = new PNJ(this,580,235,'pnj',"J'ai entendu dire que vous cherchiez à acquérir le titre de noble. Souhaiteriez vous acheté la terre seigneuriale nouvellement libre ?");
+    this.pnj6 = new PNJ(this,880,240,'pnj',"On dit que Honorable homme chercherait à séduire la fille du roi");
+    this.pnj6 = new PNJ(this,280,400,'pnj',"J'ai entendu dire que le roi cherchait un chaussetier personne. Se faire engager par le roi ?");
+
+
       // Création du bouton
     const button = this.add.text(400, 50, 'Game Over', { fill: '#0f0' }).setOrigin(0.5);
     button.setInteractive({ useHandCursor: true });
@@ -31,6 +41,7 @@ export default class ThirdGameScene extends Phaser.Scene {
     button.on('pointerdown', () => {
       this.scene.start('GameOverScene', { message: 'petanque' });
     });}
+    
 
     // Updating the scene
     // here, we update the player movements
