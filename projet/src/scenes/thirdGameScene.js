7a5e8684aa558ed3f509274cd7265d7235b7504b
Pett Vincent <vincent.pett@etu.unistra.fr>
2023-04-05 17:17:14 +0200

    Ajout du prototype du diagramme de classe, et changement du nom des variables

--- projet/src/scenes/thirdGameScene.js
+++ projet/src/scenes/thirdGameScene.js
@@ -26,11 +26,11 @@ export default class ThirdGameScene extends Phaser.Scene {
     this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bgscene');
     this.player = new Player(this, 100, 450);
 
-    this.pnj6 = new PNJ(this,640,450,'pnj',"Me defierez-vous à une partie de pétanque !?");
-    this.pnj6 = new PNJ(this,130,250,'pnj',"J'ai entendu dire que vous avez décidé de consacrer votre vie à la spiritualité. Seriez vous intéressé pas le fait de devenir moine ?");
-    this.pnj6 = new PNJ(this,580,235,'pnj',"J'ai entendu dire que vous cherchiez à acquérir le titre de noble. Souhaiteriez vous acheté la terre seigneuriale nouvellement libre ?");
-    this.pnj6 = new PNJ(this,880,240,'pnj',"On dit que Honorable homme chercherait à séduire la fille du roi");
-    this.pnj6 = new PNJ(this,280,400,'pnj',"J'ai entendu dire que le roi cherchait un chaussetier personne. Se faire engager par le roi ?");
+    this.pnj1 = new PNJ(this,640,450,'pnj',"Me defierez-vous à une partie de pétanque !?");
+    this.pnj2 = new PNJ(this,130,250,'pnj',"J'ai entendu dire que vous avez décidé de consacrer votre vie à la spiritualité. Seriez vous intéressé pas le fait de devenir moine ?");
+    this.pnj3 = new PNJ(this,580,235,'pnj',"J'ai entendu dire que vous cherchiez à acquérir le titre de noble. Souhaiteriez vous acheté la terre seigneuriale nouvellement libre ?");
+    this.pnj4 = new PNJ(this,880,240,'pnj',"On dit que Honorable homme chercherait à séduire la fille du roi");
+    this.pnj5 = new PNJ(this,280,400,'pnj',"J'ai entendu dire que le roi cherchait un chaussetier personne. Se faire engager par le roi ?");
 
 
       // Création du bouton
